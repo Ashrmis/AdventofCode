c005a22ab025fb4c8827e80a5748bb23f697a5d1
Ashish Mistry <ashishmistry19@yahoo.com>
2022-12-08 22:12:27 -0500

    day8, works for test data but not for puzzle input :(

---
+++ day8/day8.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  8 11:40:57 2022
+
+@author: AMistry
+"""
+import numpy as np
+grid = [list(map(int, line)) for line in open('day8_input.txt').read().splitlines()]
+gridarr=np.array(grid)
+
+tree_coordinates={}
+edge={}
+trees=0
+
+
+for r in range(gridarr.shape[0]):
+    for c in range(gridarr.shape[1]):
+        curr_h=gridarr[r,c]
+        if (gridarr.shape[0]-1>r>=1 and gridarr.shape[1]-1>c>=1):
+            if all(curr_h > x for x in gridarr[r,c+1:]) or all(curr_h > x for x in gridarr[r,:c]):
+                tree_coordinates[(r+1,c+1)]=(gridarr[r,c],"INTERIOR")
+            if all(curr_h > x for x in gridarr[:r,c]) or all(curr_h > x for x in gridarr[r:,c]):
+                tree_coordinates[(r+1,c+1)]=(gridarr[r,c],"INTERIOR")
+
+
+        else: # edges
+            tree_coordinates[(r,c)]=(gridarr[r,c],"EDGE")
+    
+
+print(len(tree_coordinates))
+
+# part 1 1859
+# part 2 332640
